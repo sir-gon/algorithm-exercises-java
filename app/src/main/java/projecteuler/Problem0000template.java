7412773a79fdee519ace8f6e6275ee2cb950ae06
Gonzalo Diaz <gonzalo@gon.cl>
2023-05-09 02:17:34 -0400

    Problem template

---
+++ app/src/main/java/projecteuler/Problem0000template.java
@@ -0,0 +1,13 @@
+package projecteuler;
+
+public class Problem0000template {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public Class<Integer> problem0000template() {
+        Class<Integer> result = null;
+      
+        return result;
+    }
+}
---
+++ app/src/test/java/projecteuler/Problem0000templateTest.java
@@ -0,0 +1,13 @@
+package projecteuler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem0000templateTest {
+    @Test void problem0000template() {
+        Problem0000template classUnderTest = new Problem0000template();
+        assertNull(classUnderTest.problem0000template(), 
+        "Null");
+    }
+}
