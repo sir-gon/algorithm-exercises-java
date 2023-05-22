0fa3ddd6581563e3eec34bf953f8031ae9b18dd2
Gonzalo Diaz <gonzalo@gon.cl>
2023-05-22 15:29:45 -0400

    Helper: Prime. Checks if a number is prime.

---
+++ app/src/test/java/projecteuler/helper/PrimeTest.java
@@ -0,0 +1,67 @@
+package projecteuler.helper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+
+class PrimeTest {
+  class IsPrimeTestCase {
+    Integer input;
+    Boolean answer;
+
+    IsPrimeTestCase(Integer input, Boolean answer) {
+      this.input = input;
+      this.answer = answer;
+    }
+  }
+
+  @Test void instanceCaseTest() {
+    Prime classUnderTest = new Prime();
+
+    assertNotNull(classUnderTest);
+  }
+
+  @Test void notPrimeBorderCasesCaseTest() {
+
+    IsPrimeTestCase[] testCases = {
+      new IsPrimeTestCase(0, false),
+      new IsPrimeTestCase(1, false)
+    };
+
+    for (IsPrimeTestCase testCase : testCases) {
+      assertEquals(testCase.answer, Prime.isPrime(testCase.input));
+    }
+
+  }
+
+  @Test void somePrimeNumbersCaseTest() {
+
+    IsPrimeTestCase[] testCases = {
+      new IsPrimeTestCase(2, true),
+      new IsPrimeTestCase(7, true),
+      new IsPrimeTestCase(13, true),
+    };
+
+    for (IsPrimeTestCase testCase : testCases) {
+      assertEquals(testCase.answer, Prime.isPrime(testCase.input));
+    }
+
+  }
+
+  @Test void someNotPrimeNumbersCaseTest() {
+
+    IsPrimeTestCase[] testCases = {
+      new IsPrimeTestCase(4, false),
+      new IsPrimeTestCase(10, false),
+      new IsPrimeTestCase(100, false),
+      new IsPrimeTestCase(300, false),
+    };
+
+    for (IsPrimeTestCase testCase : testCases) {
+      assertEquals(testCase.answer, Prime.isPrime(testCase.input));
+    }
+
+  }
+}
