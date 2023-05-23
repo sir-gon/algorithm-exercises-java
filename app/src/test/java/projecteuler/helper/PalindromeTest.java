5f45f66acdc31090024abd69b4a0dabe952528d5
Gonzalo Diaz <gonzalo@gon.cl>
2023-05-23 14:25:25 -0400

    New helper: Palindrome, to test if a number is a palindrome (it reads same in both directions)

---
+++ app/src/test/java/projecteuler/helper/PalindromeTest.java
@@ -0,0 +1,44 @@
+package projecteuler.helper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+
+class PalindromeTest {
+
+  private class PalindromTestCase {
+    Long input;
+    Boolean answer;
+
+    PalindromTestCase(Long input, Boolean answer) {
+      this.input = input;
+      this.answer = answer;
+    }
+  }
+
+  @Test void instanceCaseTest() {
+    Palindrome classUnderTest = new Palindrome();
+
+    assertNotNull(classUnderTest);
+    assertInstanceOf(
+        Palindrome.class,
+        classUnderTest,
+        String.format("Must be an instance of %s", Palindrome.class));
+  }
+
+  @Test void listOfPalindromBorderCaseTest() {
+
+    PalindromTestCase[] testCases = {
+      new PalindromTestCase(1L, true)
+    };
+
+    for (PalindromTestCase testCase : testCases) {
+      assertEquals(testCase.answer, Palindrome.isPalindrome(testCase.input));
+    }
+  }
+
+
+}
