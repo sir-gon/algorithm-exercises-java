c6af20f2d34cfdf61cf8faa2d0c749e92d471c13
Gonzalo Diaz <gonzalo@gon.cl>
2023-07-02 16:34:01 -0400

    Problem 0016: solved.

---
+++ app/src/test/java/projecteuler/Problem0016Test.java
@@ -0,0 +1,34 @@
+package projecteuler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class Problem0016Test {
+
+  @Test void problem0016Instance() {
+    Problem0016 classUnderTest = new Problem0016();
+
+    assertNotNull(classUnderTest);
+    assertInstanceOf(
+        Problem0016.class,
+        classUnderTest,
+        String.format("Must be an instance of %s", Problem0016.class)
+    );
+  }
+
+  @Test void problem0016() {
+
+    Integer base = 2;
+    Integer exponent = 1000;
+    String solutionExpected = "1366";
+
+    String solutionFound = Problem0016.problem0016(base, exponent);
+
+    assertEquals(solutionExpected, solutionFound,
+        String.format("Problem 0016 answer must be: %s", solutionExpected)
+    );
+  }
+}
