85d899323da5bcf9f4509fdfcbdf31ab4ac82e44
Gonzalo Diaz <gonzalo@gon.cl>
2023-07-08 13:19:28 -0400

    Problem 0020: solved.

---
+++ app/src/main/java/projecteuler/Problem0020.java
@@ -0,0 +1,42 @@
+// CHECKSTYLE.OFF: JavadocParagraph
+/**
+ * Factorial digit sum
+ *
+ * https://projecteuler.net/problem=20
+ *
+ * n! means n × (n − 1) × ... × 3 × 2 × 1
+ *
+ * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ *
+ * Find the sum of the digits in the number 100!
+ */
+
+package projecteuler;
+
+import projecteuler.helper.BigNum;
+
+/**
+ * Problem 0020.
+ */
+public class Problem0020 {
+
+  static java.util.logging.Logger logger = projecteuler.util.CustomLogger.getLogger();
+
+  /**
+   * Problem 0020.
+   */
+  public static BigNum problem0020(int limit) {
+    BigNum factorial = BigNum.bigFactorial(limit);
+
+    logger.info(String.format("Factorial of %d!: %s", limit, factorial.toString()));
+
+    BigNum result = new BigNum(0).bigSumMany(factorial.toArray());
+
+    logger.info(String.format("Problem 00020 solved: %s", result.toString()));
+
+    return result;
+  }
+}
+
+//CHECKSTYLE.ON: JavadocParagraph
