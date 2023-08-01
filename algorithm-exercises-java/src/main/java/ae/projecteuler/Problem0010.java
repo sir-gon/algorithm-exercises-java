99f6153e1e8b2cac82ee416085cfad633162bc1b
Gonzalo Diaz <gonzalo@gon.cl>
2023-08-01 02:29:31 -0400

    [REFACTOR] sonarlint fixes

--- algorithm-exercises-java/src/main/java/ae/projecteuler/Problem0010.java
+++ algorithm-exercises-java/src/main/java/ae/projecteuler/Problem0010.java
@@ -13,6 +13,7 @@ package ae.projecteuler;
 
 import ae.projecteuler.helper.NaturalNumber;
 import ae.projecteuler.helper.Sum;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 
 /**
@@ -28,8 +29,9 @@ public class Problem0010 {
    * Problem 0010.
    */
   public static Long problem0010(Long bottom, Long top) {
-    ArrayList<Long> primes = new ArrayList<Long>();
+    ArrayList<Long> primes = new ArrayList<>();
 
+    String log;
     Long i = bottom;
 
     do {
@@ -37,14 +39,17 @@ public class Problem0010 {
 
       if (new NaturalNumber(i).isPrime()) {
         primes.add(i);
-        logger.fine(String.format("Prime found %d put in position: $d", i, primes.size()));
+        log = MessageFormat.format("Prime found {0} put in position: {1}", i, primes.size());
+        logger.fine(log);
       }
     } while (i < top);
 
     Long result = Sum.sum(primes.toArray(Long[]::new));
 
-    logger.info(String.format("primes count: %d", primes.size()));
-    logger.info(String.format("Sum of primes below %d is: %d}", top, result));
+    log = MessageFormat.format("primes count: {0}", primes.size());
+    logger.info(log);
+    log = MessageFormat.format("Sum of primes below {0} is: {1}", top, result);
+    logger.info(log);
 
     return result;
   }
