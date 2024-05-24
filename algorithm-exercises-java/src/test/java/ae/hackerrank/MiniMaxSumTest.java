85c4d5b364f736b8b4e5099f1d667d4d46d69b84
Gonzalo Diaz <devel@gon.cl>
2024-05-24 18:07:35 -0400

    [Hacker Rank] Warm up: Mini-Max Sum solved ✓

---
+++ algorithm-exercises-java/src/test/java/ae/hackerrank/MiniMaxSumTest.java
@@ -0,0 +1,76 @@
+package ae.hackerrank;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class MiniMaxSumTest {
+
+  public class MiniMaxSumTestCase {
+    public List<Integer> input;
+    public String expected;
+
+    public MiniMaxSumTestCase(List<Integer> _input, String _expected) {
+      this.input = _input;
+      this.expected = _expected;
+    }
+  }
+
+  public List<MiniMaxSumTestCase> testCases;
+
+  @BeforeAll
+  public void setup() {
+    this.testCases = Arrays.asList(
+      new MiniMaxSumTestCase(Arrays.asList(1, 2, 3, 4, 5), "10 14"),
+      new MiniMaxSumTestCase(Arrays.asList(5, 4, 3, 2, 1), "10 14")
+    );
+  }
+
+  @Test
+  void testMiniMaxSum() {
+
+    for (MiniMaxSumTestCase testCase : this.testCases) {
+      String solutionFound = MiniMaxSum.miniMaxSum(testCase.input);
+
+      assertEquals(testCase.expected, solutionFound,
+          String.format(
+              "CompareTriplets.compareTriplets() answer must be: %s",
+              testCase.expected.toString()));
+    }
+  }
+
+  @Test
+  void testMiniMaxSumNullInput() {
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      MiniMaxSum.miniMaxSum(null);
+    });
+
+    String expectedMessage = "Parameter cannot be empty";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+
+  @Test
+  void testMiniMaxSumEmptyInput() {
+    List<Integer> input = Arrays.asList();
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      MiniMaxSum.miniMaxSum(input);
+    });
+
+    String expectedMessage = "Parameter cannot be empty";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+}
