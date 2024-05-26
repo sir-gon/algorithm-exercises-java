dcf4f4d56924c84c7c1ddc216e49c21c466ce81a
Gonzalo Diaz <devel@gon.cl>
2024-05-26 19:16:20 -0400

    [Hacker Rank] Warm up: Staircase solved ✓

---
+++ algorithm-exercises-java/src/main/java/ae/hackerrank/warmup/Staircase.java
@@ -0,0 +1,39 @@
+package ae.hackerrank.warmup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Staricase.
+ *
+ * @link Problem definition [[docs/hackerrank/warmup/staircase.md]]
+ */
+public class Staircase {
+
+  private Staircase() {
+  }
+
+  static java.util.logging.Logger logger = util.CustomLogger.getLogger();
+
+  /**
+   * staircase.
+   */
+  public static String staircase(int n) {
+    List<String> result = new ArrayList<>();
+
+    for (int i = 1; i < n + 1; i++) {
+      StringBuilder line = new StringBuilder();
+
+      for (int j = 1; j < n + 1; j++) {
+        if (j <= n - i) {
+          line.append(" ");
+        } else {
+          line.append("#");
+        }
+      }
+
+      result.add(line.toString());
+    }
+    return String.join("\n", result);
+  }
+}
