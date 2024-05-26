9ca56ad5e3eb925048d8067df3a3da16f0aeae84
Gonzalo Diaz <devel@gon.cl>
2024-05-26 13:48:02 -0400

    [Hacker Rank] Warm up: Plus Minus solved ✓

---
+++ algorithm-exercises-java/src/main/java/ae/hackerrank/PlusMinus.java
@@ -0,0 +1,47 @@
+package ae.hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Plus Minus.
+ *
+ * @link Problem definition [[docs/hackerrank/warmup/plus_minus.md]]
+ */
+public class PlusMinus {
+
+  private PlusMinus() {
+  }
+
+  static java.util.logging.Logger logger = util.CustomLogger.getLogger();
+
+  /**
+   * plusMinus.
+   */
+  public static String plusMinus(List<Integer> arr) {
+
+    int positives = 0;
+    int negatives = 0;
+    int zeros = 0;
+
+    for (int x : arr) {
+      if (x > 0) {
+        positives += 1;
+      }
+      if (x < 0) {
+        negatives += 1;
+      }
+      if (x == 0) {
+        zeros += 1;
+      }
+    }
+
+    List<String> result = new ArrayList<>();
+    result.add(String.format(Locale.ROOT, "%.6f", (double) positives / arr.size()));
+    result.add(String.format(Locale.ROOT, "%.6f", (double) negatives / arr.size()));
+    result.add(String.format(Locale.ROOT, "%.6f", (double) zeros / arr.size()));
+
+    return String.join("\n", result);
+  }
+}
---
+++ algorithm-exercises-java/src/test/java/ae/hackerrank/PlusMinusTest.java
@@ -0,0 +1,48 @@
+package ae.hackerrank;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class PlusMinusTest {
+
+  public class PlusMinusTestCase {
+    public List<Integer> input;
+    public String expected;
+
+    public PlusMinusTestCase(List<Integer> _input, String _expected) {
+      this.input = _input;
+      this.expected = _expected;
+    }
+  }
+
+  public List<PlusMinusTestCase> testCases;
+
+  @BeforeAll
+  public void setup() {
+    this.testCases = Arrays.asList(
+        new PlusMinusTestCase(
+            Arrays.asList(-4, 3, -9, 0, 4, 1),
+            String.join("\n", "0.500000", "0.333333", "0.166667")));
+  }
+
+  @Test
+  void testPlusMinus() {
+
+    for (PlusMinusTestCase testCase : this.testCases) {
+      String solutionFound = PlusMinus.plusMinus(testCase.input);
+
+      assertEquals(testCase.expected, solutionFound,
+          String.format("%s(%s) answer must be: %s",
+              "PlusMinus.plusMinus",
+              testCase.input.toString(),
+              testCase.expected.toString()));
+    }
+  }
+}
