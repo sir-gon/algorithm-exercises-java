30f7e65d5844194c58ff226546065c72e9ea30e3
Gonzalo Diaz <devel@gon.cl>
2024-05-27 13:59:39 -0400

    [Hacker Rank] Warm up: Birthday Cake Candles solved ✓

---
+++ algorithm-exercises-java/src/main/java/ae/hackerrank/warmup/BirthdayCakeCandles.java
@@ -0,0 +1,41 @@
+package ae.hackerrank.warmup;
+
+import java.util.List;
+
+/**
+ * Birthday Cake Candles.
+ *
+ * @link Problem definition [[docs/hackerrank/warmup/solve_me_first.md]]
+ */
+public class BirthdayCakeCandles {
+
+  private BirthdayCakeCandles() {
+  }
+
+  static java.util.logging.Logger logger = util.CustomLogger.getLogger();
+
+  /**
+   * birthdayCakeCandles.
+   */
+  public static int birthdayCakeCandles(List<Integer> candles) {
+    if (candles == null || candles.isEmpty()) {
+      throw new IllegalArgumentException("Parameter cannot be empty");
+    }
+
+    int counter = 0;
+    int maximum = candles.get(0);
+
+    for (Integer element : candles) {
+      if (element > maximum) {
+        maximum = element;
+        counter = 1;
+      } else {
+        if (element == maximum) {
+          counter += 1;
+        }
+      }
+    }
+
+    return counter;
+  }
+}
---
+++ algorithm-exercises-java/src/test/java/ae/hackerrank/warmup/BirthdayCakeCandlesTest.java
@@ -0,0 +1,82 @@
+package ae.hackerrank.warmup;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+
+@TestInstance(Lifecycle.PER_CLASS)
+class BirthdayCakeCandlesTest {
+  public class BirthdayCakeCandlesTestCase {
+    public List<Integer> candles;
+    public Integer expected;
+
+    public BirthdayCakeCandlesTestCase(List<Integer> _candles, Integer _expected) {
+      this.candles = _candles;
+      this.expected = _expected;
+    }
+  }
+
+  public List<BirthdayCakeCandlesTestCase> testCases;
+
+  @BeforeAll
+  public void setup() {
+    this.testCases = Arrays.asList(
+      new BirthdayCakeCandlesTestCase(
+        Arrays.asList(3, 2, 1, 3),
+        2
+      ),
+      new BirthdayCakeCandlesTestCase(
+        Arrays.asList(1, 2, 3, 3),
+        2
+      )
+    );
+  }
+
+  @Test void birthdayCakeCandles() {
+    for (BirthdayCakeCandlesTestCase testCase : this.testCases) {
+      Integer solutionFound = BirthdayCakeCandles.birthdayCakeCandles(testCase.candles);
+
+      assertEquals(testCase.expected, solutionFound,
+          String.format("%s(%s) answer must be: %d",
+            "CompareTriplets.compareTriplets",
+            testCase.candles.toString(),
+            testCase.expected)
+      );
+    }
+  }
+
+  @Test
+  void testMiniMaxSumNullInput() {
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      BirthdayCakeCandles.birthdayCakeCandles(null);
+    });
+
+    String expectedMessage = "Parameter cannot be empty";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+
+  @Test
+  void testMiniMaxSumEmptyInput() {
+    List<Integer> input = Arrays.asList();
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      BirthdayCakeCandles.birthdayCakeCandles(input);
+    });
+
+    String expectedMessage = "Parameter cannot be empty";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+}
