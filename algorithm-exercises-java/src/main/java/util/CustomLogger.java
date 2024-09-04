4d08009e3214cc3c3f19c047edd83855857ba003
Gonzalo Diaz <devel@gon.cl>
2024-09-04 15:14:40 -0400

    [REFACTOR] Custom Logger: Class refactor, singleton encapsulated in a new class with new static (shorcut) methods. Preserve the interface.

--- algorithm-exercises-java/src/main/java/util/CustomLogger.java
+++ algorithm-exercises-java/src/main/java/util/CustomLogger.java
@@ -1,74 +1,100 @@
 package util;
 
 /**
- * CustomLogger.
- *
- * @link https://www.logicbig.com/tutorials/core-java-tutorial/logging/levels.html
+ * Log.
  */
 public class CustomLogger {
 
-  private CustomLogger() {}
-
-  public static final String LOGGER_NAME = "projecteuler";
-  private static java.util.logging.Logger log;
-
   /**
-   * Common logger singleton.
+   * LoggerSingleton.
+   *
+   * @link https://www.logicbig.com/tutorials/core-java-tutorial/logging/levels.html
    */
-  @SuppressWarnings({"java:S106", "java:S4792"})
-  public static java.util.logging.Logger getLogger() {
+  private class LoggerSingleton {
 
-    // default log level
-    String logLevel = System.getenv("LOG_LEVEL");
+    private LoggerSingleton() {}
 
-    if (logLevel == null) {
-      logLevel = "INFO";
-    }
+    public static final String LOGGER_NAME = "projecteuler";
+    private static java.util.logging.Logger log;
 
-    logLevel = logLevel.toUpperCase();
+    /**
+     * Common logger singleton.
+     */
+    @SuppressWarnings({"java:S106", "java:S4792"})
+    public static java.util.logging.Logger getLogger() {
 
-    // Singleton initalization
-    if (log == null) {
-      log = java.util.logging.Logger.getLogger(CustomLogger.LOGGER_NAME);
+      // default log level
+      String logLevel = System.getenv("LOG_LEVEL");
 
-      java.util.logging.Level level;
-
-      switch (logLevel) {
-        case "INFO":
-          level = java.util.logging.Level.INFO;
-          break;
-        case "DEBUG":
-          level = java.util.logging.Level.FINE;
-          break;
-        case "FINE":
-          level = java.util.logging.Level.FINE;
-          break;
-        case "WARN":
-          level = java.util.logging.Level.WARNING;
-          break;
-        case "WARNING":
-          level = java.util.logging.Level.WARNING;
-          break;
-        case "ERROR":
-          level = java.util.logging.Level.SEVERE;
-          break;
-        case "SEVERE":
-          level = java.util.logging.Level.SEVERE;
-          break;
-        default:
-          level = java.util.logging.Level.INFO;
+      if (logLevel == null) {
+        logLevel = "INFO";
       }
 
-      System.out.println("Setting LOG_LEVEL: " + level.getName());
+      logLevel = logLevel.toUpperCase();
 
-      log.setLevel(level);
+      // Singleton initalization
+      if (log == null) {
+        log = java.util.logging.Logger.getLogger(LoggerSingleton.LOGGER_NAME);
 
-      log.info("INFO LEVEL ENABLED");
-      log.warning("WARNING LEVEL ENABLED");
-      log.fine("FINE LEVEL ENABLED");
-      log.finest("FINEST LEVEL ENABLED");
+        java.util.logging.Level level;
+
+        switch (logLevel) {
+          case "INFO":
+            level = java.util.logging.Level.INFO;
+            break;
+          case "DEBUG":
+            level = java.util.logging.Level.FINE;
+            break;
+          case "FINE":
+            level = java.util.logging.Level.FINE;
+            break;
+          case "WARN":
+            level = java.util.logging.Level.WARNING;
+            break;
+          case "WARNING":
+            level = java.util.logging.Level.WARNING;
+            break;
+          case "ERROR":
+            level = java.util.logging.Level.SEVERE;
+            break;
+          case "SEVERE":
+            level = java.util.logging.Level.SEVERE;
+            break;
+          default:
+            level = java.util.logging.Level.INFO;
+        }
+
+        System.out.println("Setting LOG_LEVEL: " + level.getName());
+
+        log.setLevel(level);
+
+        log.info("INFO LEVEL ENABLED");
+        log.warning("WARNING LEVEL ENABLED");
+        log.fine("FINE LEVEL ENABLED");
+        log.finest("FINEST LEVEL ENABLED");
+      }
+
+      return log;
     }
+  }
 
-    return log;
+  public static java.util.logging.Logger getLogger() {
+    return LoggerSingleton.getLogger();
+  }
+
+  public static void info(String message) {
+    LoggerSingleton.getLogger().log(java.util.logging.Level.INFO, message);
+  }
+
+  public static void warning(String message) {
+    LoggerSingleton.getLogger().log(java.util.logging.Level.WARNING, message);
+  }
+
+  public static void error(String message) {
+    LoggerSingleton.getLogger().log(java.util.logging.Level.SEVERE, message);
+  }
+
+  public static void debug(String message) {
+    LoggerSingleton.getLogger().log(java.util.logging.Level.FINE, message);
   }
 }
