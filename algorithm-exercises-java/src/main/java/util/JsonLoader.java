fd7d9d46c17faf33d5004fe412807f0835bf81b0
Gonzalo Diaz <devel@gon.cl>
2024-09-05 12:46:36 -0400

    [FEATURE] New utility to encapsulate JSON loading.

---
+++ algorithm-exercises-java/src/main/java/util/JsonLoader.java
@@ -0,0 +1,32 @@
+package util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * JsonLoader.
+ */
+public final class JsonLoader {
+
+  private JsonLoader() {}
+
+  /**
+   * loadJson.
+   */
+  public static <T> List<T> loadJson(String path, Class<T> type) throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    File file = new File(
+        JsonLoader.class.getClassLoader()
+        .getResource(path)
+        .getFile()
+    );
+
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.readerForListOf(type)
+        .readValue(objectMapper.readTree(file));
+  }
+
+}
