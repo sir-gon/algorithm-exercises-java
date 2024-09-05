bc36665dab67e36739494a1985316e613a424746
Gonzalo Diaz <devel@gon.cl>
2024-09-05 17:38:56 -0400

    [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Two Strings. Solve ✅.

---
+++ algorithm-exercises-java/src/main/java/ae/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/TwoStrings.java
@@ -0,0 +1,41 @@
+package ae.hackerrank.interview_preparation_kit.dictionaries_and_hashmaps;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * TwoStrings.
+ *
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/two-strings.md]]
+ * @link Solution notes [[docs/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/two-strings-solution-notes.md]]
+ */
+public class TwoStrings {
+  private TwoStrings() {}
+
+  private static final String YES = "YES";
+  private static final String NO = "NO";
+
+  /**
+   * twoStringsCompute.
+   */
+  public static boolean twoStringsCompute(String s1, String s2) {
+
+    Set<String> dictionary = new HashSet<>();
+
+    for (char occurrence : s1.toCharArray()) {
+      dictionary.add("%c".formatted(occurrence));
+    }
+
+    for (char occurrence : s2.toCharArray()) {
+      if (dictionary.contains("%c".formatted(occurrence))) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static String twoStrings(String s1, String s2) {
+    return twoStringsCompute(s1, s2) ? YES : NO;
+  }
+}
