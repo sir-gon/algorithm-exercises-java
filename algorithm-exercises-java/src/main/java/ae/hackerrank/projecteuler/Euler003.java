0eb7d1f0ad2147a4085c3b2f079baad2cc52045d
Gonzalo Diaz <devel@gon.cl>
2024-09-25 15:34:50 -0300

    [Hacker Rank]: Project Euler #3: Largest prime factor. Solved ✓

---
+++ algorithm-exercises-java/src/main/java/ae/hackerrank/projecteuler/Euler003.java
@@ -0,0 +1,45 @@
+package ae.hackerrank.projecteuler;
+
+/**
+ * Even Fibonacci numbers.
+ *
+ * @link Problem definition [[docs/hackerrank/projecteuler/euler003.md]]
+ */
+public class Euler003 {
+
+  private Euler003() {}
+
+  private static Long primeFactor(Long n) {
+    if (n < 2) {
+      return null;
+    }
+
+    Long divisor = n;
+    Long maxPrimeFactor = null;
+
+    int i = 2;
+    while (i <= Math.sqrt(divisor)) {
+      if (divisor % i == 0) {
+        divisor = divisor / i;
+        maxPrimeFactor = divisor;
+      } else {
+        i += 1;
+      }
+    }
+
+    if (maxPrimeFactor == null) {
+      return n;
+    }
+
+    return maxPrimeFactor;
+  }
+
+  /**
+   * Even Fibonacci numbers.
+   */
+  public static Long euler003(long n) {
+    return primeFactor(n);
+  }
+}
+
+//CHECKSTYLE.ON: JavadocParagraph
