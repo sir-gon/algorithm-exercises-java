fcffa4ab15e561ea663c6ed0b3660c0087126679
Gonzalo Diaz <devel@gon.cl>
2025-07-04 00:42:46 -0400

    [Hacker Rank] Interview Preparation Kit: Greedy Algorithms: Max Min. Solved ✓.

---
+++ algorithm-exercises-java/src/main/java/ae/hackerrank/interview_preparation_kit/greedy_algorithms/AngryFlorist.java
@@ -0,0 +1,35 @@
+package ae.hackerrank.interview_preparation_kit.greedy_algorithms;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * AngryFlorist.
+ *
+ * @link Problem definition
+ *       [[docs/hackerrank/interview_preparation_kit/greedy_algorithms/angry-children.md]]
+ */
+public class AngryFlorist {
+
+  private AngryFlorist() {
+  }
+
+  /**
+   * maxMin.
+   */
+  public static int maxMin(int k, List<Integer> arr) {
+    List<Integer> sortedNums = arr.stream().collect(Collectors.toList());
+    Collections.sort(sortedNums);
+
+    int result = sortedNums.get(sortedNums.size() - 1) - sortedNums.get(0);
+
+    for (int i = 0; i < sortedNums.size() - k + 1; i++) {
+      int tmin = sortedNums.get(i);
+      int tmax = sortedNums.get(i + k - 1);
+      result = Math.min(result, tmax - tmin);
+    }
+
+    return result;
+  }
+}
